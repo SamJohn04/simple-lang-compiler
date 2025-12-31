3a9772647d25c8bee720d31f4fa48e3a3b1c9fb9
Samuel John <samuel.john.codes@gmail.com>
2025-12-31 08:57:34 +0530

    feat: add code-generator code for assignment operators

--- internal/backend/intercode.go
+++ internal/backend/intercode.go
@@ -1,11 +1,153 @@
 package backend
 
-import "github.com/SamJohn04/simple-lang-compiler/internal/common"
+import (
+	"fmt"
 
-// INTEND returns 3-Address Code
+	"github.com/SamJohn04/simple-lang-compiler/internal/common"
+)
+
+var (
+	numberOfIdentifiers int
+	identifierData      map[string]int
+	stack               []int
+)
+
+// returns 3-Address Code
 func IntermediateCodeGenerator(input common.SyntaxTreeNode) ([]string, error) {
+	numberOfIdentifiers = 0
+	identifierData = make(map[string]int)
+	stack = []int{}
+
+	intermediateCodes := []string{}
+	for _, child := range input.ChildNodes {
+		codesFromChild, err := generateNextInstructionSet(child)
+		if err != nil {
+			return []string{}, err
+		}
+		intermediateCodes = append(intermediateCodes, codesFromChild...)
+	}
+	return intermediateCodes, nil
+}
+
+func generateNextInstructionSet(input common.SyntaxTreeNode) ([]string, error) {
+	switch input.InnerToken.TokenKind {
+	case common.TokenAssignment:
+		// v = E
+		codesFromChild, outputVariable, err := generateForExpression(input.ChildNodes[1])
+		if err != nil {
+			return []string{}, err
+		}
+		codesFromChild = append(
+			codesFromChild,
+			fmt.Sprintf("%v = %v", input.InnerToken.Token, outputVariable),
+		)
+
+	case common.TokenIf:
+		// if R { I } I4
+
+	case common.TokenWhile:
+		// while R { I }
+
+	case common.TokenOutput:
+		// output E
+
+	default:
+		return []string{}, intermediateCodeGeneratorInternalError(
+			fmt.Sprintf(
+				"Unexpected token received: %v",
+				common.NameMapWithTokenKind[input.InnerToken.TokenKind],
+			),
+		)
+	}
 	return []string{}, &common.UnderConstructionError{
 		PointOfFailure: "Intermediate Code Generator",
 		Message:        "",
+	}
+}
+
+func generateForExpression(input common.SyntaxTreeNode) ([]string, string, error) {
+	switch input.InnerToken.TokenKind {
+	case common.TokenLiteralInt:
+		return []string{}, input.InnerToken.Token, nil
+	case common.TokenIdent:
+		return []string{}, input.InnerToken.Token, nil
+	case common.TokenInput:
+		identifier := getNextIdentifier()
+		codes := []string{
+			fmt.Sprintf(
+				"%v = input",
+				identifier,
+			),
+		}
+		return codes, identifier, nil
+
+	case common.TokenExpressionSub:
+		if len(input.ChildNodes) == 1 {
+			// t = - t
+			childCodes, outputVariable, err := generateForExpression(input.ChildNodes[0])
+			if err != nil {
+				return []string{}, "", err
+			}
+
+			nextIdentifier := getNextIdentifier()
+			childCodes = append(
+				childCodes,
+				fmt.Sprintf("%v = - %v", nextIdentifier, outputVariable),
+			)
+			return childCodes, nextIdentifier, nil
+		}
+		fallthrough
+	case common.TokenExpressionAdd:
+		fallthrough
+	case common.TokenExpressionMul:
+		fallthrough
+	case common.TokenExpressionDiv:
+		fallthrough
+	case common.TokenExpressionModulo:
+		// t = t op t
+		firstChildCodes, firstOutputVariable, err := generateForExpression(input.ChildNodes[0])
+		if err != nil {
+			return []string{}, "", err
+		}
+		secondChildCodes, secondOutputVariable, err := generateForExpression(input.ChildNodes[1])
+		if err != nil {
+			return []string{}, "", err
+		}
+
+		nextIdentifier := getNextIdentifier()
+		codes := []string{}
+		codes = append(codes, firstChildCodes...)
+		codes = append(codes, secondChildCodes...)
+		codes = append(
+			codes,
+			fmt.Sprintf(
+				"%v = %v %v %v",
+				nextIdentifier,
+				firstOutputVariable,
+				common.Operators[input.InnerToken.TokenKind],
+				secondOutputVariable,
+			),
+		)
+		return codes, nextIdentifier, nil
+
+	default:
+		return []string{}, "", intermediateCodeGeneratorInternalError(
+			fmt.Sprintf(
+				"unknown token at expression %v",
+				common.NameMapWithTokenKind[input.InnerToken.TokenKind],
+			),
+		)
+	}
+}
+
+func getNextIdentifier() string {
+	numberOfIdentifiers++
+	return fmt.Sprintf("t%d", numberOfIdentifiers)
+}
+
+func intermediateCodeGeneratorInternalError(message string) *common.InternalError {
+	return &common.InternalError{
+		PointOfFailure: "Intermediate Code Generator",
+		Message:        message,
 	}
 }
